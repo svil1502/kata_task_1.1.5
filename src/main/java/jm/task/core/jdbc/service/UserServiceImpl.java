de673eb7575cf010ec33175a1f3593fbf4ae575a
Svetlana Ilina <developersvil@gmail.com>
2023-04-17 15:42:05 +0300

    Change after mentor 2

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -6,7 +6,7 @@ import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
-public class UserServiceImpl implements UserService, UserDao {
+public class UserServiceImpl implements UserService {
     UserDao userDao = new UserDaoJDBCImpl();
     public void createUsersTable() {
 
